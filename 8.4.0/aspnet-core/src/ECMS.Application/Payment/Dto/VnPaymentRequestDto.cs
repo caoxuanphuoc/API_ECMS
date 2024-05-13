aaa617595126fb920fbd2c2fa52b7a2e7d6e55e8
Phuoc <caophuoc6@gmail.com>
2024-05-14 00:45:40 +0700

    Add APi payment Bug

--- 8.4.0/aspnet-core/src/ECMS.Application/Payment/Dto/VnPaymentRequestDto.cs
+++ 8.4.0/aspnet-core/src/ECMS.Application/Payment/Dto/VnPaymentRequestDto.cs
@@ -8,10 +8,10 @@ namespace ECMS.Payment.Dto
 {
     public class VnPaymentRequestDto
     {
-        public int OrderId { get; set; }
+        public long OrderId { get; set; }
         public string FullName { get; set; }
         public string Description { get; set; }
-        public double Amount { get; set; }
+        public decimal Amount { get; set; }
         public DateTime CreatedDate { get; set; }
     }
 }
--- 8.4.0/aspnet-core/src/ECMS.Web.Core/Controllers/PaymentControler.cs
+++ 8.4.0/aspnet-core/src/ECMS.Web.Core/Controllers/PaymentControler.cs
@@ -1,4 +1,13 @@
-﻿using ECMS.Users.Dto;
+﻿using ECMS.Authorization.Users;
+using ECMS.Order;
+using ECMS.Order.Const;
+using ECMS.OrderDomain.Order;
+using ECMS.OrderDomain.Order.Dto;
+using ECMS.OrderDomain.Order.Dto.ValidateOrderDto;
+using ECMS.Payment;
+using ECMS.Payment.Dto;
+using ECMS.Users.Dto;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -8,11 +17,47 @@ using System.Threading.Tasks;
 
 namespace ECMS.Controllers
 {
-    public class PaymentControler
+        [Route("api/[controller]/[action]")]
+    public class PaymentControler: ECMSControllerBase
     {
-        public ActionResult<string> PaymentCallback()
+        private readonly HttpContext _context;
+        private readonly IPaymentAppService _paymentService;
+        private readonly IOrderAppService _orderAppService;
+        public PaymentControler(HttpContext context,
+            IPaymentAppService paymentService,
+            OrderAppService orderAppService)
         {
-            return "";
+            _context = context;
+            _paymentService = paymentService;
+            _orderAppService = orderAppService;
+        }
+        [HttpGet]
+        public async Task<string> PaymentConfirm(ValidateOrderDto input)
+        {
+            decimal discount = input.Fee / 10;
+            CreateOrderDto dataOrder = new CreateOrderDto
+            {
+                UserId=  input.UserId,
+                ClassId = input.ClassId,
+                Cost = input.Fee,
+                Status = StatusOrder.Pending.ToString(),
+                TotalDiscount = discount,
+                TotalCost = input.Fee - discount
+
+            };
+            // create Order with status = 0 (Pending)
+            var orderData = await _orderAppService.CreateAsync(dataOrder);
+
+            VnPaymentRequestDto dataVnPay = new VnPaymentRequestDto {
+            OrderId = orderData.Id,
+            FullName = input.FullName,
+            Description = $"Thanh Toán hóa đơn {orderData.OrderCode}",
+            Amount = orderData.TotalCost,
+            CreatedDate = DateTime.Now,
+
+            };
+            var url = _paymentService.GetPaymentRequest(_context, dataVnPay);
+            return url;
 
         }
     }
