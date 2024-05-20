b58cf9cd0e36a42df378745f16064edfe6151e34
Phuoc <caophuoc6@gmail.com>
2024-05-20 23:59:06 +0700

    add user aftter crate order

--- 8.4.0/aspnet-core/src/ECMS.Application/OrderDomain/OrderHistories/OrderHistoryAppService.cs
+++ 8.4.0/aspnet-core/src/ECMS.Application/OrderDomain/OrderHistories/OrderHistoryAppService.cs
@@ -11,32 +11,46 @@ using ECMS.OrderDomain.OrderHistories.Dto;
 using Abp.Domain.Repositories;
 using ECMS.Order.Const;
 using Abp.UI;
+using ECMS.UserClasses;
+using ECMS.UserClasses.Dto;
+using ECMS.Authorization.Users;
+using ECMS.Classes.UserClass;
 
 namespace ECMS.OrderDomain.OrderHistories
 {
     public class OrderHistoryAppService : AsyncCrudAppService<OrderHistory, OrderHistoryDto, long, PagedOrderHistoryResultRequestDto, CreateOrderHistoryDto, UpdateOrderHistoryDto>, IOrderHistoryAppService
     {
         private readonly IRepository<Orders, long> _orderRepo;
+        private readonly IUserClassAppService _userClassAppService;
         public OrderHistoryAppService(IRepository<OrderHistory, long> repository, IRepository<Orders, long> orderRepo) : base(repository)
         {
             _orderRepo = orderRepo;
         }
         public override async Task<OrderHistoryDto> CreateAsync(CreateOrderHistoryDto input)
         {
-              var order = await _orderRepo.FirstOrDefaultAsync(x => x.OrderCode == input.OrderCode);
-              if( input.ResponseCode == 200 )
-              {
-                  order.Status = StatusOrder.Success;
-              }
-              else
-              {
-                  order.Status = StatusOrder.Cancel;
-              }
+            var order = await _orderRepo.FirstOrDefaultAsync(x => x.OrderCode == input.OrderCode);
+            if (input.ResponseCode == 200)
+            {
+                order.Status = StatusOrder.Success;
 
-              
-              await CurrentUnitOfWork.SaveChangesAsync();
+                CreateUserClassDto adduser = new CreateUserClassDto
+                {
+                    UserId = order.UserId,
+                    ClassId = order.ClassId,
+                    IsActive = true,
+                    RoleMember = TypeRole.Student
+
+                };
+                _userClassAppService.CreateAsync(adduser);
+              }
+            else
+            {
+                order.Status = StatusOrder.Cancel;
+            }
+
+            await CurrentUnitOfWork.SaveChangesAsync();
             var check = await Repository.FirstOrDefaultAsync(x => x.TransactionNo == input.TransactionNo);
-                if(check != null )
+            if (check != null)
             {
                 throw new UserFriendlyException("Đơn hàng đã được xác nhận");
             }
